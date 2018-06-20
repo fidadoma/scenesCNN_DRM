496c7562f119ce64d4139a4726d8cc770d5e9db9
fidadoma <filip.dechterenko@gmail.com>
2018-06-20 15:52:13 +0200

    reformat code for protocols

--- R/create_protocol.R
+++ R/create_protocol.R
@@ -24,8 +24,12 @@ df_files <- df_files %>% dplyr::mutate(id = 1:n())
 gist$id <- 1:nrow(gist)
 
 n <- 100
+n_points <- 9 # how many images will be there in grid
 
 df_files_sample <- df_files %>% sample_n(n)
+
+p <- create_empty_protocol(1)
+
 
 for (i in 1:n) {
   
@@ -37,31 +41,13 @@ for (i in 1:n) {
   
   gist_point <- gist[r$id, ] %>% select(-id) %>% as.matrix()
   
-  cl2 <- select_points_close_to_centerL2(gist[this_cat_ix,], gist_point)
-  cl3 <- select_points_farthest_from_centerL2(gist[this_cat_ix,], gist_point)
+  gr_close <- select_points_close_to_centerL2(gist[this_cat_ix,], gist_point, n_points = n_points)
+  gr_far <- select_points_farthest_from_centerL2(gist[this_cat_ix,], gist_point, n_points = n_points)
   
-  df_for_plot <- rbind(cl2[-9, ], cl3[1, ])
-  
-  df_for_plot <- df_for_plot %>% left_join(df_files, by = "id")
-  
-  df_for_plot <- df_for_plot %>%
-    rowwise() %>%
-    dplyr::mutate(img_data = list(jpeg::readJPEG(pth))) %>%
-    ungroup()
-  
-  # MDS check
-  
-  mds_onecat <- cmdscale(dist(df_for_plot %>% select(-(id:img_data)))) %>% as.data.frame()
-  colnames(mds_onecat) <- c("x","y")
-  mds_onecat$close <- T
-  mds_onecat$close[df_for_plot$id == cl3$id] <- F
-  
-  p <- mds_onecat %>%
-    ggplot(aes(x, y, col = close)) +
-    geom_point() +
-    theme(aspect.ratio = 1)
-  ggsave(file.path(outdir, sprintf("%s_mds_gist.png", tools::file_path_sans_ext(r$filename))), p)
-  
+  # replace the most distant point in close points with the most distant one
+  df_for_plot <- rbind(gr_close[-n_points, ], gr_far[1, ]) %>% 
+    left_join(df_files, by = "id")
+
   # we can't use those images for next trials
   keep_ix <- !(df_files$id %in% df_for_plot$id)
   df_files <- df_files[keep_ix,]
