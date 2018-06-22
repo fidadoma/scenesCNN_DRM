96e2be1070a36e8069a73c22ad5ef63452b3d005
fidadoma <filip.dechterenko@gmail.com>
2018-06-22 09:20:28 +0200

    change paths for multiple computers

--- R/07_copyKonkledatabase.R
+++ R/07_copyKonkledatabase.R
@@ -5,8 +5,17 @@ library(FDhelpers)
 
 load(here::here("data","konkle_180621.RData"))
 
-out_dir <- "g:/konkle"
-konkle_dir <- "g:/konkle_raw"
+
+if(Sys.info()[[4]] == "FIDADOMA") {
+  out_dir <- "D:/documents/databases/konkle"
+  konkle_dir <- "D:/documents/databases/konkle_raw"
+} else if (Sys.info()[[4]] == "LUKAVSKY-PSU") {
+  out_dir <- "g:/konkle"
+  konkle_dir <- "g:/konkle_raw"
+  
+} else {
+  stop("Unknown workstation")
+}
 
 tm <- create.time.measure(nrow(image_info))
 for(i in 1:nrow(image_info)) {
