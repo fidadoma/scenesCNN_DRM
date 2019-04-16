404062124b63b2de2cea73a1d189b1be5a093db2
fidadoma <filip.dechterenko@gmail.com>
2019-04-16 20:40:55 +0200

    start changes for protocol creation

--- R/DRM_exp/exp_DRM_create_protocols.R
+++ R/DRM_exp/exp_DRM_create_protocols.R
@@ -14,12 +14,17 @@ p0 <- read_csv(file.path(prot_dir, "P000.csv"))
 nProt <- 20
 
 for (i in 1:nProt) {
+  p0 %>% 
+    group_by(id_rep) %>% 
+    filter(type == "target") %>% 
+    sample_frac(1)
   p <- p0 %>% 
     group_by(id_rep) %>% 
     sample_frac(1) %>% 
     ungroup() %>% 
     mutate(trial_id = 1:n()) %>% 
     mutate(prot_id = i) %>% 
-    select(prot_id,trial_id, everything())
+    mutate(corr_answer = if_else(type == "target", "left", "right")) %>% 
+    select(prot_id, trial_id, everything())
   write_csv(p, file.path(prot_dir, sprintf("P%03d.csv", i)))
 }
