f633c0645a0a7847ea4f91d20e4f9c125538259f
fidadoma <filip.dechterenko@gmail.com>
2019-04-16 15:26:07 +0200

    create protocols for experiment

---
+++ R/DRM_exp/exp_DRM_create_protocols.R
@@ -0,0 +1,25 @@
+# load figrim data and prepare protocols 
+
+set.seed(190416)
+
+library(tidyverse)
+
+source(here::here("R","utils.R"))
+
+prot_dir <- here::here("data","exp_DRM1","protocols")
+stopifnot(dir.exists(prot_dir))
+
+p0 <- read_csv(file.path(prot_dir, "P000.csv"))
+
+nProt <- 20
+
+for (i in 1:nProt) {
+  p <- p0 %>% 
+    group_by(id_rep) %>% 
+    sample_frac(1) %>% 
+    ungroup() %>% 
+    mutate(trial_id = 1:n()) %>% 
+    mutate(prot_id = i) %>% 
+    select(prot_id,trial_id, everything())
+  write_csv(p, file.path(prot_dir, sprintf("P%03d.csv", i)))
+}
