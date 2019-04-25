7c37e98c9940c2e47c6a483b2c0af789ae5cc769
fidadoma <filip.dechterenko@gmail.com>
2019-04-25 17:44:37 +0200

    start of analysis of DRM exp

---
+++ R/DRM_exp/01_prepare_results.R
@@ -0,0 +1,20 @@
+# read and merge datafiles
+
+library(tidyverse)
+library(here)
+
+data_pth <- here("data/exp_DRM1/results")
+
+fs <- list.files(data_pth, full.names = T, pattern = "*.csv")
+
+df <- fs %>%
+  map(~ read_csv(.)) %>% 
+  reduce(rbind)
+
+df_resp <- df %>% 
+  select(-X31) %>% 
+  filter(query == 1) %>% 
+  select(subject_id = participant, prot_id:corrKey,key_resp = key_resp_4.keys, correct = key_resp_4.corr, rt_key = key_resp_4.rt,
+         confidence = rating.response, confidence.rt = rating.rt, date)
+  
+saveRDS(df_resp, file = here("data/exp_DRM1/results_190425.rds"))
