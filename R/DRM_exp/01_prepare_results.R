b717c2260880786f0fbabcb39bc933d1c4d06dfa
fidadoma <filip.dechterenko@gmail.com>
2019-04-26 19:39:01 +0200

    use confidence for analysis

--- R/DRM_exp/01_prepare_results.R
+++ R/DRM_exp/01_prepare_results.R
@@ -15,6 +15,9 @@ df_resp <- df %>%
   select(-X31) %>% 
   filter(query == 1) %>% 
   select(subject_id = participant, prot_id:corrKey,key_resp = key_resp_4.keys, correct = key_resp_4.corr, rt_key = key_resp_4.rt,
-         confidence = rating.response, confidence.rt = rating.rt, date)
+         confidence = rating.response, confidence.rt = rating.rt, date) %>% 
+  mutate(corrKey = recode(corrKey, left = "old", right = "new"),
+         key_resp = recode(key_resp, left = "old", right = "new")) %>% 
+  filter(!is.na(correct))
   
 saveRDS(df_resp, file = here("data/exp_DRM1/results_190425.rds"))
