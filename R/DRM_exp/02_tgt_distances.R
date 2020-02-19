8f8be070a2698b2ca6afb015a99706cbefce9d6a
fidadoma <filip.dechterenko@gmail.com>
2020-02-19 18:45:32 +0100

    add tgt ditances for review

---
+++ R/DRM_exp/02_tgt_distances.R
@@ -0,0 +1,70 @@
+
+set.seed(190216)
+
+library(tidyverse)
+library(here)
+source(here::here("R","utils.R"))
+
+#load("data/m_figrim_l2.RData")
+load(here::here("data","file_info.RData"))
+load(here::here("data","figrim_vectors.RData"))
+
+# we are using cleaned database, we need to clean the figrim data structure as well
+
+df_files <- df_files %>% filter(filename %in% df_figrim_fc7$filename)
+
+prot_dir <- here::here("data","exp_DRM1","protocols")
+
+if(!dir.exists(prot_dir)){
+  stop("Missing protocols")
+}
+
+fs <- list.files(prot_dir, full.names = T, pattern = "*.csv") %>% setdiff(file.path(prot_dir,"P000.csv"))
+
+df_protocols <- fs %>%
+  map(~ read_csv(., col_types = cols(
+    prot_id = col_double(),
+    trial_id = col_double(),
+    grp_order = col_double(),
+    category = col_character(),
+    id_rep = col_double(),
+    img_name = col_character(),
+    type = col_character(),
+    query = col_double(),
+    corrKey = col_character()
+  ))) %>% 
+  reduce(rbind)
+
+save_plots <- T
+
+plots_dir <- here::here("plots","exp_DRM1","image_spaces")
+
+if(!dir.exists(plots_dir)) {
+  dir.create(plots_dir, recursive = T)
+}
+
+
+categories <- df_files$category %>% unique()
+
+
+df <- readRDS(here("data", "exp_DRM1", "results_200219.rds"))
+
+df_participants <- readxl::read_excel(here::here("data","exp_DRM1","participants_190509.xlsx"))
+
+n_targets_per_category <- 15 
+tm <- FDhelpers::create.time.measure(nrow(df))
+for (i in 1:nrow(df)) {
+  ix_check <- (1:15)+15*(((df$trial_id[i]-1) %/% 15)-1)
+  p_prot <- df_protocols %>% filter(prot_id == df$prot_id[i])
+  p_prot_check <- p_prot %>% filter(trial_id %in% ix_check)
+  curr_img <- df_figrim_fc7 %>% filter(filename == df$img_name[i])
+  tgt_imgs <- df_figrim_fc7 %>% filter(filename %in% p_prot_check$img_name)
+  
+  curr_img_vals <- curr_img %>% select(starts_with("V")) %>% as.matrix()
+  categ_img_vals <- tgt_imgs %>% select(starts_with("V")) %>% as.matrix()
+  df$dist_to_tgts[i] <- list(l2norm(categ_img_vals,curr_img_vals))
+  tm <- FDhelpers::update.tm(tm)
+  FDhelpers::print.tm(tm)
+}
+
+df %>% group_by()
